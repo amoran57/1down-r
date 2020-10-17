2f223620b665bc63d6c9ce1d7d7dd041fe05c43a
Alex Moran <alexander.moran99@gmail.com>
2020-10-16 21:34:27 -0400

    Initial commit

---
+++ 1down.R
@@ -0,0 +1,14 @@
+times <- 1000000
+iterations <- c()
+
+for (i in 1:times) {
+  r <- 1000000
+  x <- 0
+  while(r != 1) {
+    r <- round(runif(1, 0.5, r + 0.5))
+    x <- (x+1)
+  }
+  iterations <- c(iterations, x)
+}
+
+hist(iterations)
